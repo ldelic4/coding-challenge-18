4c6d3890024f35bb792e201d537b865e6be13055
Lea Delic <leadelic0429@gmail.com>
2024-12-08 19:10:42 -0500

    Add reusable ChartComponent

---
+++ chartcomponent.jsx
@@ -0,0 +1,32 @@
+//task 2
+
+import React, { useEffect, useRef } from 'react';
+import Chart from 'chart.js/auto';
+//define function component andensures proper cleanup
+const ChartComponent = ({ type, data, options }) => {
+    const chartRef = useRef(null);
+    const chartInstanceRef = useRef(null);
+    //use effect and accesses 2d rendering
+    useEffect(() => {
+        const ctx = chartRef.current.getContext('2d');
+        if (chartInstanceRef.current) {
+            chartInstanceRef.current.destroy();
+          }
+          //define data set
+          chartInstanceRef.current = new Chart(ctx, {
+            type,
+            data,
+            options,
+          });
+          return () => {
+            if (chartInstanceRef.current) {
+              chartInstanceRef.current.destroy();
+              chartInstanceRef.current = null;
+            }
+          };
+        }, [type, data, options]);
+        //html element where the chart will be rendered
+        return <canvas ref={chartRef}></canvas>
+        
+    };
+    export default ChartComponent;
